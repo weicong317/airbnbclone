d80af07b08a035e3f90ef30a028295683f1196f2
weicong317 <weicong317@gmail.com>
2018-07-18 11:39:07 +0800

    changed production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'google.com',
+    user_name:            ENV['GOOGLE_USER_NAME'],
+    password:             ENV['GOOGLE_PASSWORD'],
+    authentication:       'login',
+    enable_starttls_auto: true  }
 end
