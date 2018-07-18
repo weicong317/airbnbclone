e3b094ad7a7fcdf6dda4b3005cc604ef70d111ad
weicong317 <weicong317@gmail.com>
2018-07-18 09:52:02 +0800

    added filter and ajax feature to listing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,28 @@
 //= require_tree .
 //= require moment
 //= require tempusdominus-bootstrap-4.js
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  let searchCountryArray = [];
+  $( "#search_country" ).keyup(function(key) {
+    $.ajax({
+      method: "GET",
+      url: "/search/autocomplete",
+      data: $(this).serialize(),
+      dataType: "json"
+    })
+    .done(function( data ) {
+      if (data.length === 0 || ($("#search_country").val().length === 0 && (key.keyCode === 8 || key.keyCode === 46)) ) {
+        $( "#dropdown" ).empty(); 
+        searchCountryArray = [];           
+      } else {
+        data.forEach(function(element) {
+          if (searchCountryArray.indexOf(element.country) === -1) {
+            searchCountryArray.push(element.country);
+            $( "#dropdown" ).append( `<option value=${element.country}>` );
+          };
+        });
+      };
+    });
+  });
+});
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,12 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
 
+  scope :country, -> (country) { where(country: country) }
+  scope :price, -> (price) { where("price <= ?", price) }
+  scope :amenities, -> (amenities) { where("amenities @> ARRAY[?]", amenities) }
+  scope :verification, -> { where(verification: true) }
+  scope :starts_with, -> (country) { where("country ILIKE ?", "#{country}%")}
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
